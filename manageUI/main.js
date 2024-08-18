8a1ff4d573862800e34f2dea7aefe3cfcc684b99
Dincz <dineshndr02@gmail.com>
2024-08-18 15:51:13 +0530

    5th part

--- manageUI/main.js
+++ manageUI/main.js
@@ -5,8 +5,11 @@ const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible") 
-  ? navToggle.setAttribute("aria-expended", false)
-  : navToggle.setAttribute("aria-expended", true); 
+  ? navToggle.setAttribute("aria-expanded", false)
+  : navToggle.setAttribute("aria-expanded", true); 
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
+
+
+
